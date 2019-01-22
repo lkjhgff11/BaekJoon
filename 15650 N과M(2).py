86eed828c36c4a6ca9857d7d176163b7512123b8
ruru <lkjhgff11@naver.com>
2019-01-22 20:46:44 +0900

    N과M시리즈(2)

--- 15650 N과M(2).py
+++ 15650 N과M(2).py
@@ -1,16 +1,14 @@
 n,m=map(int,input().split())
 
-def rec(index=0,nums=[],src=1):
+def rec(index=0,nums=[],i=1):
     if index==m:
         print(*nums)
         
-    for i in range(src,n+1):
+    for i in range(i,n+1):
         if i in nums:
             continue
         
         nums.append(i)
         rec(index+1,nums,i+1)
         nums.pop(-1)
-
 rec()
-    
